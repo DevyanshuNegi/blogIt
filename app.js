14c3f0388c8d6b0e9ed724af2f26db53c4f3b09a
DevyanshuNegi <devyanshunegi@gmail.com>
2024-05-02 08:05:42 +0530

    small changes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+import expressEjsLayouts from "express-ejs-layouts";
+// for express layouts
+
 const app = express()
 
 app.use(cors({
@@ -19,15 +22,23 @@ app.use(express.static("public")) // for images and ...
 app.use(cookieParser()) // for storing and reading safe cookies
 
 
-app.get("/home", (req, res)=> {
-    // res.send("HEllo world")
-    res.render("pages/home.ejs")
-})
+// Use EJS as the view engine
+app.set('view engine', 'ejs');
 
+// Other middleware and route handlers...
 
 // import userRouter from "./routes/user.routes.js"
 
 // app.use("/api/v1/users", userRouter)
+
+
+
+
+app.get("/home", (req, res) => {
+    // res.send("HEllo world")
+    res.render("pages/home.ejs")
+})
+
 
 export default app;
 export {app};

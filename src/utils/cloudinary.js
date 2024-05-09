cf8d7fa7ecdd690c33009b94fdd0e6bff86e07d8
DevyanshuNegi <devyanshunegi@gmail.com>
2024-05-09 23:04:37 +0530

    added multer middleware and auth middleware

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,22 +10,20 @@ cloudinary.config({
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null
+        if (!localFilePath) return null
         // upload the file on cloudinary
-        const response = cloudinary.uploader.upload
-        (localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfully
-        console.log("file is uploaded on cloudinary",
-        response.url);
+        const response = await cloudinary.uploader.upload
+            (localFilePath, {
+                resource_type: "auto"
+            })
+        // console.log("response : ", response) 
+        fs.unlinkSync(localFilePath)
         return response;
+        
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved file due to fail
         return null;
     }
 }
 
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-    { public_id: "olympic_flag" },
-    function (error, result) { console.log(result); });
+export { uploadOnCloudinary }
